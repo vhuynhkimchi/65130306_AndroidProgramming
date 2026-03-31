a9d6a22f1fa1b0954efe12981641c0cb4f156572
vhuynhkimchi <162584036+vhuynhkimchi@users.noreply.github.com>
2026-03-31 22:07:29 +0700

    Add Fragment_Ex_Dynamic

---
+++ Fragment_ExAddDynamic/app/src/main/java/kc/edu/fragment_exadddynamic/MainActivity.java
@@ -0,0 +1,25 @@
+package kc.edu.fragment_exadddynamic;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.frameContentHolder, new ContentFragment())
+                .add(R.id.frameFooterHolder, new FooterFragment())
+                .commit();
+    }
+}
